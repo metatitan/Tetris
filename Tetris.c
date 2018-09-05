5cf3c0464c648947c8d2a47d406bf0e418092381
Metatitan <crushhed_ice@gmx.de>
2018-09-05 15:45:05 +0200

    Fixed syntax errors, tetromino now stops when hitting the ground.

--- Tetris.c
+++ Tetris.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
 
-const int ROWS = 20;
-const int COLS = 10;
+#define ROWS 20
+#define COLS 10
+
+int gameboard[ROWS][COLS];
+int copiedBoard[ROWS][COLS];
 
 int tetro_o[2][2] = {
 	{1, 1},
@@ -10,84 +15,122 @@ int tetro_o[2][2] = {
 };
 
 int * copyBoard(int board[ROWS][COLS]);
-void nullBoard(int *board[ROWS][COLS]);
+void nullBoard();
 
 void drawBoard(int *board);
+int calcRows();
+int calcCols();
 
-int calcRows(int tetro[][]);
-int calcCols(int tetro[][]);
-
-int xPos = 1;
+int xPos = 0;
 int yPos = 1;
 
 //some comment for git
-++
+char currentTetro = 'o';
+
+int running = 1;
+
+
 
 
 int main(){
 
-	int gameboard[ROWS][COLS];
 
 	int *cb;
-	int *tetro = &tetro_o;
+	int *tetro = &(tetro_o[0][0]);
 
-	while(running){
+	struct timespec tim, tim2;
+  tim.tv_sec = 0;
+  tim.tv_nsec = 500000000L;
+
+	nullBoard();
+
+	while(running == 1){
 
 		system("clear");
 
 		cb = copyBoard(gameboard);
 
-		for(i = 0; i < calcRows(*tetro); i++){
-			for(j = 0; j < calcCols(*tetro); j++){
-				*(cb + (j + yPos) + (i + xPos) * COLS) += *tetro[i][j];
+		for(int i = 0; i < calcRows(); i++){
+			for(int j = 0; j < calcCols(); j++){
+				//printf("%d\n", (cb + (j + yPos) + (i + xPos) * COLS));
+				int sum = *(cb + (j + yPos) + (i + xPos) * COLS) + *(tetro + j + i * calcCols());
+				if((xPos + calcRows()) == ROWS){
+					printf("Reached bottom\n");
+					gameboard[i + xPos][j + yPos] = sum;
+				}else if(sum == 2){
+					printf("Reached another one");
+					xPos = xPos - 1;
+					gameboard[i + xPos][j + yPos] = sum;
+				}
+				*(cb + (j + yPos) + (i + xPos) * COLS) = sum;
 			}
 		}
 
-		drawBoard();
+		//running = 0;
+		if(xPos < ROWS){
+			xPos+= 1;
+		}else{
+			xPos = 1;
+		}
 
 
+		drawBoard(cb);
+
+		nanosleep(&tim, &tim2);
+	}
 	return 0;
 }
 
 
 int * copyBoard(int board[ROWS][COLS]){
 
-	int sol[ROWS][COLS];
 
-	for(i = 0; i < ROWS; i++){
-		for(j = 0; j < COLS; j++){
-			sol[i][j] = board[i][j];
+	for(int i = 0; i < ROWS; i++){
+		for(int j = 0; j < COLS; j++){
+			copiedBoard[i][j] = board[i][j];
 		}
 	}
 
-	return sol;
+	return &(copiedBoard[0][0]);
 }
 
-void nullBoard(int *board[ROWS][COLS]){
-
+void nullBoard(){
+	for(int i = 0; i < ROWS; i++){
+		for(int j = 0; j < COLS; j++){
+			gameboard[i][j] = 0;
+		}
+	}
 }
 
 void drawBoard(int *board){
-	for(i = 0; i < COLS; i++){
+	for(int i = 0; i < COLS; i++){
 		printf("-");
 	}
 	printf("\n");
-	for(i = 0; i < ROWS; i++){
-		for(j = 0; j < COLS; j++){
+	for(int i = 0; i < ROWS; i++){
+		for(int j = 0; j < COLS; j++){
 			if(*(board + j + i * COLS) == 0){
 				printf(" ");
+			}else if(*(board + j + i * COLS) == 1){
+				printf("0");
 			}else{
-				printf("%c", (char) 254);
+				printf("x");
 			}
 		}
 		printf("\n");
 	}
 }
 
-int calcRows(int tetro[][]){
-	return sizeof(tetro) / sizeof(tetro[0]);
+int calcRows(){
+	switch(currentTetro){
+		case 'o':
+			return 2;
+	}
 }
 
-int calcCols(int tetro[][]){
-	return sizeof(tetro[0]) / sizeof(tetro[0][0]);
+int calcCols(){
+	switch(currentTetro){
+		case 'o':
+			return 2;
+	}
 }
